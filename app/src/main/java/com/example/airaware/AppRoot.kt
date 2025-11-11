2a87fe46a3a880010d811e668c9000a339b6e3d0
4shx1 <ashwin.biju.k@gmail.com>
2025-11-11 22:15:49 +0530

    feat: Implemented splash screen
    Added a new splash screen that displays the app logo. The screen is shown for a short duration when the app starts, with a fade-out animation. A `SplashViewModel` has been added to handle future logic for checking user login status.

--- app/src/main/java/com/example/airaware/AppRoot.kt
+++ app/src/main/java/com/example/airaware/AppRoot.kt
@@ -1,17 +1,36 @@
 package com.example.airaware.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.*
 import com.example.airaware.data.local.User
 import com.example.airaware.ui.auth.AuthScreen
 import com.example.airaware.ui.home.HomeScreen
+import com.example.airaware.ui.splash.SplashScreen
+import kotlinx.coroutines.delay
 
 @Composable
 fun AppRoot() {
     var loggedInUser by remember { mutableStateOf<User?>(null) }
+    var showSplash by remember { mutableStateOf(true) }
 
-    if (loggedInUser == null) {
-        AuthScreen(onAuthSuccess = { user -> loggedInUser = user })
-    } else {
-        HomeScreen(user = loggedInUser!!, onLogout = { loggedInUser = null })
+    LaunchedEffect(Unit) {
+        delay(300) // Show splash for 1 seconds
+        showSplash = false
+    }
+
+    AnimatedVisibility(
+        visible = showSplash,
+        exit = fadeOut(animationSpec = androidx.compose.animation.core.tween(durationMillis = 1000)) // 👈 fade-out
+    ) {
+        SplashScreen()
+    }
+
+    if (!showSplash) {
+        if (loggedInUser == null) {
+            AuthScreen(onAuthSuccess = { user -> loggedInUser = user })
+        } else {
+            HomeScreen(user = loggedInUser!!, onLogout = { loggedInUser = null })
+        }
     }
 }
